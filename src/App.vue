0e839569758d54adea377d1dd817237526301c6b
Francesco-Munafo <acquamarea1@gmail.com>
2023-09-21 15:12:33 +0200

    Add request and bonuses using bs5

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+
+//Importo il componente "titolo"
+import titolo from './components/AppTitle.vue'
+//Importo il jumbo
+import jumbo from './components/AppHero.vue'
+//Importo la libreria di bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default{
+
+  data(){
+
+    return{
+      message: 'Hello Vite',
+      
+    }
+  },
+  components:{
+    titolo,
+    jumbo
+  }
+}
+</script>
+
+<template>
+  <titolo></titolo>
+  <h2>{{ message }}</h2>
+  <jumbo></jumbo>
+
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<template>
+    <div class="mt-4 p-5 bg-primary text-white rounded">
+        <h1>Il mio primo jumbotron con Vite + Bootstrap!</h1>
+        <p>Hello world!</p>
+    </div>
+</template>
+
+<style></style>
